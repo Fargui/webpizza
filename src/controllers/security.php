bdcc0f498c2b1c426056fddb20062ed1fa46182d
Arnaud BODEL <contact@osw3.net>
2019-07-16 17:27:36 +0200

    -

--- src/controllers/security.php
+++ src/controllers/security.php
@@ -16,22 +16,51 @@ function security_login()
 
     if ($_SERVER['REQUEST_METHOD'] === "POST")
     {
-        
+        $isValid = true;
+    
+        // Récup des données
+        $email          = isset($_POST['email']) ? trim($_POST['email']) : null;
+        $password_text  = isset($_POST['password']) ? $_POST['password'] : null;
+    
+        // Est ce que un utilisateur correspond à l'adresse $email
+        $user = getUserByEmail($email);
+
+        // Si $user est un tableau vide, => L'UTILISATEUR N'EST PAS ENREGISTRE DANS LA BDD
+        if (empty($user)) {
+            $isValid = false;
+        }
+    
+        // Si l'utilisateur a ete trouvé dans la BDD
+        // On compare le mot de passe saisi et celui de la BDD
+        if ($isValid) 
+        {
+            if (password_verify( $password_text, $user[0]['password'] )) 
+            {
+                // Recupération du panier utilisateur a partir du numero de session
+                // Ce code permet d'associer un panier à un client qui s'identifie après 
+                // que celui-ci ait créer le panier en étant anonyme
+                $order = getOrderByUser( session_id() );
+
+                // Suppression du MDP du resultat de la requete
+
+                // Ajouter les informations utilisateur dans la $_SESSION
+
+                // Associe l'ID utilisateur à sa commande en cours
+
+                // Redirige l'utilisateur
+            }
+            else {
+                $isValid = false;
+            }
+        }
+    
+        if (!$isValid) {
+            setFlashbag("danger", "oops, mauvais identifiants....");
+        }
     }
+
+    include_once "../src/views/security/login.php";
 }
-// Si utilisateur deja identifier => redirection vers /mon-compte
-// Affichage du formulaire d'identification de l'utilisateur (identifiant + mot de passe)
-// Traitement des données du formulaire
-    // Definition de $isValid = true;
-    // Récup des données
-    // Recup (dans la BDD) de la ligne correspondante à l'identifiant utilisateur
-    // Si utilisateur OK
-        // Test du mot de passe
-        // Si mot de passe OK => création de la session
-        // Recup du panier client (a faire plus tard)
-        // Si mot de passe KO => $isValid = false;
-    // Si utilisateur KO => $isValid = false;
-    // Test si !$isValid => flashbag : message d'erreur
 
 
 /**
@@ -95,15 +124,6 @@ function security_register()
 
     include_once "../src/views/security/register.php";
 }
-// Si utilisateur deja identifier => redirection vers /mon-compte
-// Affichage du formulaire (Nom + prenom + email + mot de passe + date naissance + CGV)
-// Traitement des données du formulaire
-    // Récup des données
-    // Controle des données
-    // Verification de l'unicité de l'utilisateur
-    // Enregistrement des données dans la BDD
-    // Si OK => redirection vers /connexion ou process de connexion
-    // Si KO => flashbag message d'erreur
 
 
 /**
