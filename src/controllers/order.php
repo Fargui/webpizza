614f9a31719b258ba4e042650b234de03cbe85a9
Arnaud BODEL <contact@osw3.net>
2019-07-18 16:07:53 +0200

    -

--- src/controllers/order.php
+++ src/controllers/order.php
@@ -25,6 +25,7 @@ function order_add()
 {
     // Dépendances des Models
     include_once "../src/models/products.php";
+    include_once "../src/models/order.php";
 
     // Récupération de l'ID produit
     $product_id = isset($_GET['id']) ? (int) (trim($_GET['id'])) : null;
@@ -43,9 +44,46 @@ function order_add()
     $user_sess_id = session_id();
     $user_id = isset($_SESSION['user']['id']) ? $_SESSION['user']['id'] : null;
 
+    // Récupération de la commande client
+    if ($user_id != null) {
+        $order = getOrderByUser($user_id);
+    } else {
+        $order = getOrderByUser($user_sess_id);
+    }
+
+    // Création de la commande (si non existante)
+    if (!$order) 
+    {
+        $order = createOrder([
+            "session" => $user_sess_id,
+            "id" => $user_id
+        ]);
+    } 
+    // Ou récupération de l'id de la commande
+    else 
+    {
+        $order = $order->id;
+    }
+
+    // Récupération des produits dans la commande
+    $order_products = getOrderProducts($order);
+
+    // Si la requete retourne FALSE
+    if (!$order_products) {
+        // On surcharge $order_products avec un tableau vide
+        $order_products = [];
+    }
+
+    // On déclare la variable $addProductToOrder avec la valeur TRUE, qui va nous permettre de 
+    // savoir si on ajoute le produit dans la BDD ou non
+    $addProductToOrder = true;
+
+
+
     dump( $product );
     dump( $user_sess_id );
     dump( $user_id );
+    var_dump( $order_products );
 
 }
 
---
+++ src/models/order.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Récupère une commande en fonction de l'id utilisateur ou la session PHP
+ *
+ * @param int|string $user ID user ou PHP Sess ID
+ * @return void
+ */
+function getOrderByUser($user) 
+{
+    global $db;
+
+    // Définition de la requete
+    $sql = "SELECT `id`,`amount` FROM `order` WHERE `id_user`=:user || `sess_user`=:user";
+    // Préparation de la requete
+    $query = $db['main']->prepare($sql);
+    $query->bindValue(':user', $user);
+
+    // Execution de la requete
+    $query->execute();
+
+    // Resultat de la requete
+    return $query->fetch(PDO::FETCH_OBJ);
+}
+
+
+/**
+ * Creation d'un commande liée à un utilisateur
+ *
+ * @param [type] $user
+ * @return void
+ */
+function createOrder($user)
+{
+    global $db;
+
+    // Définition de la requete
+    $sql = "INSERT INTO `order` (`id_user`, `sess_user`) VALUES (:id_user, :sess_user)";
+
+    // Oréparation de la requete
+    $query = $db['main']->prepare($sql);
+    $query->bindValue(':id_user', $user['id']);
+    $query->bindValue(':sess_user', $user['session'], PDO::PARAM_STR);
+
+    // Execution de la requete
+    $query->execute();
+
+    // Retourne l'id de la commande
+    return $db['main']->lastInsertId();
+}
+
+
+/**
+ * Retourne la liste des produits d'une commande
+ *
+ * @param integer $id_order ID de la commande
+ * @return void
+ */
+function getOrderProducts($id_order)
+{
+    global $db;
+    
+    // définition de la requete
+    $sql = "SELECT `id`,`id_product`,`quantity`,`price` FROM `order_product` WHERE `id_order`=:id_order";
+
+    // Préparattion de la requete
+    $query = $db['main']->prepare($sql);
+    $query->bindValue(':id_order', $id_order, PDO::PARAM_INT);
+
+    // Execution de la requete
+    $query->execute();
+
+    return $query->fetchAll(PDO::FETCH_OBJ);
+}
